1716c6e8b412f79c182648bb8447706d6a598cbf
Anuj khare <anujkhare92@gmail.com>
2025-12-19 02:10:10 +0530

    'dummy-code'

---
+++ src/test/java/com/anuj_khare/Lovable/LovableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuj_khare.Lovable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
